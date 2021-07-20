c17b34084b29a156d646478d5279ff88f5e0b868
William Crews <crewswh@gmail.com>
2021-07-20 10:06:05 -0400

    somewhat better...?

--- public/script/app.js
+++ public/script/app.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+// const express = require('express');
+// const exphbs = require('express-handlebars');
 
-const app = express();
+const { Server } = require("http")
 
-app.engine('hbs', exphbs({
-    defaultLayout: 'main',
-    extname: '.hbs'
-}));
+// const app = express();
 
-app.set('view engine', 'hbs');
+// app.engine('hbs', exphbs({
+//     defaultLayout: 'main',
+//     extname: '.hbs'
+// }));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+// app.set('view engine', 'hbs');
 
-app.listen(3333, () => {
-    console.log('The web server has started on PORT 3333');
-});
+// app.get('/', (req, res) => {
+//     res.render('home');
+// });
+
+// app.listen(3333, () => {
+//     console.log('The web server has started on PORT 3333');
+// });
+
+// ^^^  All of this is in the Server.  ^^^
