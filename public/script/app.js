fc2579dd8867b2132b73bbfded86c2a48392db40
Noah King <noah@horizon3.ai>
2021-07-20 17:57:52 -0400

    push

--- public/script/app.js
+++ public/script/app.js
@@ -1,7 +1,7 @@
 // const express = require('express');
 // const exphbs = require('express-handlebars');
 
-const { Server } = require("http")
+// const { Server } = require("http")
 
 // const app = express();
 
