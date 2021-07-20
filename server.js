41d7ef88da34a4a7bc7194348cb98a5760362089
William Crews <crewswh@gmail.com>
2021-07-19 20:27:44 -0400

    workinggit add .

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const hbs = exphbs.create({});
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3333;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
