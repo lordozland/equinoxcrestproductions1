4ab3893a54cac5090f050bd28d3c8f58d713e6d0
William Crews <crewswh@gmail.com>
2021-07-19 23:56:20 -0400

    finished for the night

--- controllers/dish-routes.js
+++ controllers/dish-routes.js
@@ -19,6 +19,18 @@ router.get('/life', async (req, res) => {
     res.render('life');
   });
 
+  router.get('/sound', async (req, res) => {
+    res.render('sound');
+  });
+
+  router.get('/design', async (req, res) => {
+    res.render('design');
+  });
+
+  router.get('/company', async (req, res) => {
+    res.render('company');
+  });
+
 
 
 module.exports = router;
